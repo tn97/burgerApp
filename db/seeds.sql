b7eb0fcd626e7a182ec07531c0dec503f145d217
Timothy Na <runy385@gmail.com>
2018-11-24 17:26:48 -0500

    added test

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Beef-Steak');
+INSERT INTO burgers (name) VALUES ('White-American cheese');
+INSERT INTO burgers (name, foodcoma) VALUES ('Bacon Cheese', true);
+INSERT INTO burgers (name, foodcoma) VALUES ('Chicken', true);
+INSERT INTO burgers (name) VALUES ('Vegetarian');
